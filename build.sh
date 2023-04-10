d633abbb90b89d640cdeff94a65dde5494339364
Andre de Carvalho Cerqueira <andre.cerqueira@bndes.gov.br>
2023-04-10 10:59:06 -0300

    Configura variáveis de proxy no build

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 RBB_VERSION=${RBB_VERSION:-latest}
 RBB_IMAGE=${RBB_IMAGE:-rbb:${RBB_VERSION}}
 
-docker build --tag=rbb --network=host .
+docker build --build-arg http_proxy --build-arg https_proxy --build-arg no_proxy --tag=rbb --network=host .
