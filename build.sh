c27de2e1b7398937bb823c896393f20f33e4259d
Andre de Carvalho Cerqueira <andre.cerqueira@bndes.gov.br>
2023-04-24 16:19:26 -0300

    Redefine RBB_IMAGE para bndes/rbb

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 RBB_VERSION=${RBB_VERSION:-latest}
-RBB_IMAGE=${RBB_IMAGE:-rbb:${RBB_VERSION}}
+RBB_IMAGE=${RBB_IMAGE:-bndes/rbb:${RBB_VERSION}}
 
 docker buildx build --build-arg http_proxy --build-arg https_proxy --build-arg no_proxy --tag=${RBB_IMAGE} --network=host .
